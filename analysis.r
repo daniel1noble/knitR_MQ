7b0e6d8032cad9386900486e50ec46bcc8740c8b
Daniel Noble <daniel.wa.noble@gmail.com>
2014-11-14 17:05:55 +1100

    Data and analysis

---
+++ analysis.r
@@ -0,0 +1,9 @@
+
+
+data <- data.frame(sex = c(rep("M", 50),rep("F", 50)), x1 = rnorm(100, 34, 1), x2 = rnorm(100, 20, 1)) 
+
+dummy <- ifelse(data$sex == "M", 1, 0)
+
+data$y <- rnorm(100, data$x1*2 + dummy*5 + data$x2*5.5, 1)
+
+summary(mod <- lm(y ~ sex + x1 + x2, data = data))
