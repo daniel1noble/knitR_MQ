55c2662655231b70433496c8e3be5f5b69247fcc
Diego Barneche <diego.barneche@ymail.com>
2014-11-14 18:05:46 +1100

    modified paper

--- paper.R
+++ paper.R
@@ -2,7 +2,7 @@ library(knitr)
 knit('text/ms.Rmd', output=file.path(getwd(), 'docs/ms.md'), quiet=TRUE, encoding = 'utf-8')
 
 # to word
-system('pandoc -o docs/ms.docx docs/ms.md -s -S --bibliography library.bib --csl plos-biology.csl')
+system('pandoc -o docs/ms.docx docs/ms.md -s -S --bibliography library.bib --csl plos-biology.csl -H ms.sty')
 
 # to pdf
-system('pandoc docs/ms.md -o docs/ms.pdf -s -S --bibliography library.bib --csl plos-biology.csl')
+system('pandoc docs/ms.md -o docs/ms.pdf -s -S --bibliography library.bib --csl plos-biology.csl -H ms.sty')
