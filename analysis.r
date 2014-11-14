acac8f06057813ecd7527fc71ea7f5c3021abf05
Daniel Noble <daniel.wa.noble@gmail.com>
2014-11-14 18:02:40 +1100

    Added ms and analysis file removed old analysis file

--- analysis.r
+++ analysis.r
@@ -1,4 +1,4 @@
-
+source('R/functions.R')
 
 data <- data.frame(sex = c(rep("M", 50),rep("F", 50)), x1 = rnorm(100, 34, 1), x2 = rnorm(100, 20, 1)) 
 
@@ -6,4 +6,6 @@ dummy <- ifelse(data$sex == "M", 1, 0)
 
 data$y <- rnorm(100, data$x1*2 + dummy*5 + data$x2*5.5, 1)
 
-summary(mod <- lm(y ~ sex + x1 + x2, data = data))
+mod <- lm(y ~ sex + x1 + x2, data = data)
+
+results  <-  modelSummary(mod)
