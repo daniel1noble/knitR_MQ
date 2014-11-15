42b0c71fcab982471304f4a11a0a9906d06ed222
Diego Barneche <diego.barneche@ymail.com>
2014-11-16 01:35:12 +1100

    fixed problems with knitr compilation

--- paper.R
+++ paper.R
@@ -1,8 +1,19 @@
 library(knitr)
-knit('docs/ms.Rmd', output=file.path(getwd(), 'docs/ms.md'), quiet=TRUE, encoding = 'utf-8')
+knit('docs/ms.Rmd', output='docs/ms.md', quiet=TRUE, encoding = 'utf-8')
 
 # to word
 system('pandoc -o docs/ms.docx docs/ms.md -s -S --bibliography library.bib --csl plos-biology.csl -H ms.sty')
 
 # to pdf
-system('pandoc docs/ms.md -o docs/ms.pdf -s -S --bibliography library.bib --csl plos-biology.csl -H ms.sty')
+system('pandoc docs/ms.md -o docs/ms.tex -s -S --bibliography library.bib --csl plos-biology.csl -H ms.sty')
+x  <-  readLines('docs/ms.tex')
+# insert the float package
+x  <-  sub('(\\\\begin\\{document\\})', '\\\\usepackage{float}\n\\1', x)
+# add the H option for all figures
+x  <-  gsub('(\\\\begin\\{figure\\})', '\\1[H]', x)
+# write the processed tex file back
+writeLines(x, 'docs/ms.tex')
+# compile to pdf
+system('pandoc docs/ms.tex -o docs/ms.pdf -s -S --bibliography library.bib --csl plos-biology.csl -H ms.sty')
+system('rm -rf figure/')
+
